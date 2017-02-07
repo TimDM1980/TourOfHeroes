20a9dd09dbd85cff284862df062235ec88b255f2
bktid <Cegeka01>
2017-02-07 14:51:45 +0100

    routing: dashboard component

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,19 +4,22 @@ import { FormsModule }   from '@angular/forms';
 import { RouterModule }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
-import { HeroService } from './hero.service';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroesComponent }  from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
     FormsModule, 
     RouterModule.forRoot([
-      { path: 'heroes', component: HeroesComponent }
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+      { path: 'heroes', component: HeroesComponent },
+      { path: 'dashboard', component: DashboardComponent }
     ]) 
   ],
-  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
+  declarations: [ AppComponent, DashboardComponent, HeroesComponent, HeroDetailComponent ],
   bootstrap:    [ AppComponent ],
   providers:    [ HeroService ]
 })

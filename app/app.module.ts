f2285d98c2376b5c89aa05e4ca54a0f7a100f564
bktid <Cegeka01>
2017-02-07 20:11:46 +0100

    routing: anchor in dashboard + separate routing module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { RouterModule }   from '@angular/router';
 
+import { AppRoutingModule }  from './app-routing.module';
 import { AppComponent }  from './app.component';
 import { DashboardComponent }  from './dashboard.component';
 import { HeroesComponent }  from './heroes.component';
@@ -10,16 +10,7 @@ import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroService } from './hero.service';
 
 @NgModule({
-  imports:      [ 
-    BrowserModule, 
-    FormsModule, 
-    RouterModule.forRoot([
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'heroes', component: HeroesComponent },
-      { path: 'detail/:id', component: HeroDetailComponent },
-    ]) 
-  ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
   declarations: [ AppComponent, DashboardComponent, HeroesComponent, HeroDetailComponent ],
   bootstrap:    [ AppComponent ],
   providers:    [ HeroService ]

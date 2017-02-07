8df0aa58767631aed2402e200954df79cc8ae5c4
bktid <Cegeka01>
2017-02-07 14:39:24 +0100

    router: added route to heroes

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroService } from './hero.service';
@@ -8,9 +9,15 @@ import { HeroesComponent }  from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule, 
+    RouterModule.forRoot([
+      { path: 'heroes', component: HeroesComponent }
+    ]) 
+  ],
   declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
   bootstrap:    [ AppComponent ],
-  providers:    [HeroService]
+  providers:    [ HeroService ]
 })
 export class AppModule { }
